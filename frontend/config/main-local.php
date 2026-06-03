4fd2a4696e83cdad25a453f87440cbf236e7b2ab
Jair Jafet Caamal Yam <jair.jafer.caamal.yam@gmail.com>
2026-06-02 18:12:41 -0600

    Agregar archivos de configuracion necesarios para ejecutar Yii

---
+++ frontend/config/main-local.php
@@ -0,0 +1,25 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'Bv6zUmedOsE1WxTKgu-0OA2m23N74ifN',
+        ],
+    ],
+];
+
+if (!YII_ENV_TEST) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => \yii\debug\Module::class,
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => \yii\gii\Module::class,
+    ];
+}
+
+return $config;
